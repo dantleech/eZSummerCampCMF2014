7c3cd11348f135cd718849fe6df1c043de6ee4f9
dantleech <dan.t.leech@gmail.com>
2014-08-06 23:16:29 +0200

    [CP1] Bundles, Initializers and Data Fixtures

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
+            new Acme\BasicCmsBundle\AcmeBasicCmsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
