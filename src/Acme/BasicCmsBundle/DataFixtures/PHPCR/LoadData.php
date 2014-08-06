3954d74a5d5a623593d22aea67c5770187d731ab
dantleech <dan.t.leech@gmail.com>
2014-08-06 23:16:29 +0200

    [CP1] Bundles, Initializers and Data Fixtures

---
+++ src/Acme/BasicCmsBundle/DataFixtures/PHPCR/LoadData.php
@@ -0,0 +1,48 @@
+<?php
+
+// src/Acme/BasicCmsBundle/DataFixtures/PHPCR/LoadPageData.php
+namespace Acme\BasicCmsBundle\DataFixtures\PHPCR;
+
+use Acme\BasicCmsBundle\Document\Page;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\Yaml;
+
+class LoadData implements FixtureInterface
+{
+    public function load(ObjectManager $dm)
+    {
+        $this->loadPageData($dm);
+        $this->loadPostData($dm);
+    }
+
+    protected function loadPageData(ObjectManager $dm)
+    {
+        $parent = $dm->find(null, '/cms/pages');
+
+        $fixtureLoader = new Yaml();
+        $documents = $fixtureLoader->load(__DIR__.'/data/pages.yml');
+
+        foreach ($documents as $document) {
+            $document->setParentDocument($parent);
+            $dm->persist($document);
+        }
+
+        $dm->flush();
+    }
+
+    protected function loadPostData(ObjectManager $dm)
+    {
+        $parent = $dm->find(null, '/cms/posts');
+
+        $fixtureLoader = new Yaml();
+        $documents = $fixtureLoader->load(__DIR__.'/data/posts.yml');
+
+        foreach ($documents as $document) {
+            $document->setParentDocument($parent);
+            $dm->persist($document);
+        }
+
+        $dm->flush();
+    }
+}
